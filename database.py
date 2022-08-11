da3cf2788d25e257ebf7859eb250cb9ae2ed77b7
cuongduong <duonghaccuong.vn@gmail.com>
2022-08-11 17:07:10 +0700

    revert to deploy on pythonanywhere

--- database.py
+++ database.py
@@ -3,8 +3,8 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from env import DATABASE_URL
 
-#engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
-engine = create_engine(DATABASE_URL)
+engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+#engine = create_engine(DATABASE_URL)
 
 SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
 
