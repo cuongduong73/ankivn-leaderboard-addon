713424f45e176b84f41a4f84b3219597315bace5
cuongduong <duonghaccuong.vn@gmail.com>
2022-08-13 18:59:10 +0700

    update check permission function

--- routers/achievement.py
+++ routers/achievement.py
@@ -4,7 +4,7 @@ from sqlalchemy.orm import Session
 from form import schemas, models
 from database import get_db
 import oauth2
-from routers.helper import check_permission
+from routers.helper import ROLE_DEPUTY_AD, check_user_existed_by_name
 
 
 router = APIRouter(
@@ -14,12 +14,12 @@ router = APIRouter(
 
 @router.put("/", status_code=status.HTTP_202_ACCEPTED, response_model=schemas.SetUserAchievementResponse)
 def update(request: schemas.SetUserAchievementRequest, db: Session = Depends(get_db), current_user: str = Depends(oauth2.get_current_user)):
-    check_permission(current_user, db)
+    current_user_info = check_user_existed_by_name(current_user, db).first()
+    if current_user_info.role < ROLE_DEPUTY_AD:
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN,
+                            detail=f"User {current_user} doesn't have this permission !") 
 
-    user_info = db.query(models.User).filter(models.User.username == request.username)
-    if not user_info.first():
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
-                            detail=f"User {request.username} is not existed !")
+    user_info = check_user_existed_by_name(request.username, db)
     achievements = schemas.Achievements(gold=request.achievements.gold + user_info.first().gold,
                                         silver=request.achievements.silver + user_info.first().silver,
                                         bronze=request.achievements.bronze + user_info.first().bronze)
--- routers/authentication.py
+++ routers/authentication.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter, Depends, status, HTTPException
 from fastapi.security import OAuth2PasswordRequestForm
 from sqlalchemy.orm import Session
+from routers.helper import check_user_existed_by_name
 from security.hashing import Hash
 
 from database import get_db
@@ -11,15 +12,12 @@ router = APIRouter(tags=["Authentication"])
 
 @router.post("/login")
 def login(request: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
-    user = db.query(models.User).filter(models.User.username == request.username).first()
-    if not user:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, 
-                            detail=f"User {request.username} is not existed !")
-    if not Hash.verify(user.password, request.password):
+    user_info = check_user_existed_by_name(request.username, db).first()
+    if not Hash.verify(user_info.password, request.password):
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
                             detail=f"Incorrected password")
 
-    access_token = jwt_token.create_access_token(data={"sub": user.username})
+    access_token = jwt_token.create_access_token(data={"sub": user_info.username})
     return {
         "access_token": access_token,
         "token_type": "bearer"
